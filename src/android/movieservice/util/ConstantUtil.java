dce7a770db5708d19445f45522f2f31d3a94a99b
davidchan1420 <davidchan1420@gmail.com>
2014-07-14 18:10:52 +0800

    Update ConstantUtil.java

--- src/android/movieservice/util/ConstantUtil.java
+++ src/android/movieservice/util/ConstantUtil.java
@@ -6,7 +6,7 @@ public class ConstantUtil {
 	public static final String FORWARD_SLASH = "/";
 	
 	
-	public static final String REMOTEHOST_ANDROID = "http://10.0.2.2:1201";
+	public static final String REMOTEHOST_ANDROID = "http://10.0.2.2:1201";;;;;;;;;;;;;;;;;;;;;
 //	public static final String REMOTEHOST_ANDROID = "http://210.17.248.198:1201";
 
 

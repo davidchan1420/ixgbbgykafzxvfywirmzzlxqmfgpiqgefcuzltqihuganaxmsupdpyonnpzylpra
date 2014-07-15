7394cc6ec29631edfb7f9589d77bec4d51bc1833
roamerecom <roamerecom@localhost>
2014-07-15 16:39:07 +0800

    .

--- src/android/movieservice/util/ConstantUtil.java
+++ src/android/movieservice/util/ConstantUtil.java
@@ -8,7 +8,8 @@ public class ConstantUtil {
 	public static final String REMOTEHOST_ANDROID = "http://10.0.2.2:1201";
 //	public static final String REMOTEHOST_ANDROID = "http://210.17.248.198:1201";
 
-	public static final String NOT_AVAILABLE = "ABC";
+	public static final String NOT_AVAILABLE = "N/A";
+
 
 
 }

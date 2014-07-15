0b18ff062667c0c62775aead0a6e859e37408617
davidchan1420 <davidchan1420@gmail.com>
2014-07-15 15:57:42 +0800

    Update ConstantUtil.java

--- src/android/movieservice/util/ConstantUtil.java
+++ src/android/movieservice/util/ConstantUtil.java
@@ -9,7 +9,7 @@ public class ConstantUtil {
 //	public static final String REMOTEHOST_ANDROID = "http://210.17.248.198:1201";
 
 
-	public static final String NOT_AVAILABLE = "N/AAAA";
+	public static final String NOT_AVAILABLE = "ABC";
 	
 
 }

36986910a8add5ab837bcbf1048fe2a1fc4f9c15
roamerecom <roamerecom@localhost>
2014-12-04 17:17:10 +0800

    ...

--- src/app/movieservice/util/ConstantUtil.java
+++ src/app/movieservice/util/ConstantUtil.java
@@ -8,8 +8,8 @@ public class ConstantUtil {
 	public static final String SPACE = " ";
 	
 //	public static final String REMOTEHOST_ANDROID = "http://10.0.2.2:1201";
-//	public static final String REMOTEHOST_ANDROID = "http://202.64.49.117:1201";	//New IP
-	public static final String REMOTEHOST_ANDROID = "http://v9.hkserv.com:1201";
+	public static final String REMOTEHOST_ANDROID = "http://202.64.49.117:1201";	//New IP
+//	public static final String REMOTEHOST_ANDROID = "http://v9.hkserv.com:1201";
 
 	public static final String NOT_AVAILABLE = "N/A";
 

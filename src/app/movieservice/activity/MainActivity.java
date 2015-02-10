a7208e3cedceed8ef5f8b470db43232ac6ecf8bc
roamerecom <roamerecom@localhost>
2015-02-10 18:34:47 +0800

    Draft - Add Google Ad codes

--- src/app/movieservice/activity/MainActivity.java
+++ src/app/movieservice/activity/MainActivity.java
@@ -49,6 +49,8 @@ import app.movieservice.dialog.LanguageDialogFragment;
 import app.movieservice.util.ConstantUtil;
 
 import com.google.gson.Gson;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
 
 public class MainActivity extends Activity {
 
@@ -77,8 +79,11 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setTitle(R.string.app_name);
-		setContentView(R.layout.activity_main);		
+		setContentView(R.layout.activity_main);
 		
+		AdView mAdView = (AdView) findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);		
 
 		locationManager = (LocationManager) MainActivity.this.getSystemService(Context.LOCATION_SERVICE);
 
